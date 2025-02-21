5acc55cd87c6de2e26f58901fb12b2fb9782b4f8
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2025-02-22 02:41:29 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
